ccbcd3d4759e26b2bc9eb0a8591b5de37aea9083
szekelypeter <szekely.pter.29@gmail.com>
2016-05-03 13:59:11 +0200

    thread pool readyy

---
+++ ThreadPool/ThreaPool/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreaPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.Read();
+        }
+
+        static void ShowMyText(object state)
+        {
+            string myText = (string)state;
+            Console.WriteLine("Thread: {0} - {1}",
+            Thread.CurrentThread.ManagedThreadId, myText);
+        }
+    }
+}
